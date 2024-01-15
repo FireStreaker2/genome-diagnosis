d9be0451e7ad56e45577a534694437a29ed664e9
FireStreaker2 <michaelccc2019@outlook.com>
2024-01-14 22:03:05 -0800

    reminder page

--- app/src/main/java/com/medicationapp/FirstFragment.java
+++ app/src/main/java/com/medicationapp/FirstFragment.java
@@ -5,7 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.medicationapp.databinding.FragmentFirstBinding;
 
@@ -22,6 +24,18 @@ public class FirstFragment extends Fragment {
         binding = FragmentFirstBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(FirstFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_Reminders);
+            }
+        });
     }
 
     @Override
