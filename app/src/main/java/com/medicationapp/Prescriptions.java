f6b3f7ad12497b0193d1f3cae103374630bf1f56
HongyiRen2009 <HongyiRen2009@outlook.com>
2024-01-15 21:49:55 -0800

    message

--- app/src/main/java/com/medicationapp/Prescriptions.java
+++ app/src/main/java/com/medicationapp/Prescriptions.java
@@ -1,13 +1,20 @@
 package com.medicationapp;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -18,7 +25,9 @@ import java.util.Map;
 
 public class Prescriptions extends Fragment {
     private PrescriptionsBinding binding;
-
+    private static final int REQUEST_CAMERA_PERMISSION_CODE = 1;
+    private static final int REQUEST_IMAGE_CAPTURE = 2;
+    Button takePhoto;
     @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
@@ -27,7 +36,12 @@ public class Prescriptions extends Fragment {
 
         binding = PrescriptionsBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
-
+        view.findViewById(R.id.take_photo).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                captureImage(v);
+            }
+        });
         // development placeholders
         HashMap<String, String> patientToPrescription = new HashMap<>();
         patientToPrescription.put("bob", "funny mushrooms");
@@ -49,7 +63,16 @@ public class Prescriptions extends Fragment {
 
         return binding.getRoot();
     }
+    public void captureImage(View view){
+        if(ContextCompat.checkSelfPermission(getContext(),Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.CAMERA},REQUEST_CAMERA_PERMISSION_CODE);
+            return;
+        }
 
+        // open the camera app
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
 
     @Override
     public void onDestroyView() {
