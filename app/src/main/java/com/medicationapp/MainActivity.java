d4bac878e87ca1c60c6b2a818ab389a5af1edc23
FireStreaker2 <michaelccc2019@outlook.com>
2024-01-15 21:01:07 -0800

    refactor, nav, cleaning

--- app/src/main/java/com/medicationapp/MainActivity.java
+++ app/src/main/java/com/medicationapp/MainActivity.java
@@ -2,9 +2,7 @@ package com.medicationapp;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,14 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
-import com.google.android.material.snackbar.Snackbar;
-import com.medicationapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -43,16 +35,32 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FirstFragment()).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
             navigationView.setCheckedItem(R.id.nav_home);
         }
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // items for navigation
+        // make sure to add a new case
+        // whenever a new page is added
         switch (item.getItemId()) {
-            case R.id.nav_home:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FirstFragment()).commit();
+            case R.id.nav_prescriptions:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Prescriptions()).commit();
+                break;
+
+            case R.id.nav_reminders:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Reminders()).commit();
+                break;
+
+            case R.id.nav_orders:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Orders()).commit();
+                break;
+
+            default:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Home()).commit();
                 break;
         }
 
